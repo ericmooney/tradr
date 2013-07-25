9a0f802c486be8558bae54bf405f68d80639ac52
Eric Mooney <eric.c.mooney@gmail.com>
2013-07-24 19:26:00 -0700

    Add ajax signup and login

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -4,13 +4,18 @@ class UserSessionsController < ApplicationController
 
   def new
     @user = User.new
+
+    respond_to do |format|
+      format.html # new.html.erb
+      format.js
+    end
   end
 
   def create
     respond_to do |format|
-      if @user = login(params[:username],params[:password])
+      if @user = login(params[:email],params[:password])
         format.html { redirect_back_or_to(:users, :notice => 'Login successful.') }
-        format.xml { render :xml => @user, :status => :created, :location => @user }
+        format.js
       else
         format.html { flash.now[:alert] = "Login failed."; render :action => "new" }
         format.xml { render :xml => @user.errors, :status => :unprocessable_entity }
